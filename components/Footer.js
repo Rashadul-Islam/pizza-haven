191ce3fe7297975324c0977860fecf7f209527bb
unknown <rashadulislam4581@gmail.com>
2022-08-08 18:54:28 +0600

    frontend complete

--- components/Footer.js
+++ components/Footer.js
@@ -10,29 +10,29 @@ const Footer = () => {
       <div className={styles.item}>
         <div className={styles.card}>
           <h2 className={styles.motto}>
-            OH YES, WE DID.THE LAMA PIZZA, WELL BAKED SLICE OF PIZZA.
+            Visit our restaurant and have our delicious pizza. Order now!!!
           </h2>
         </div>
         <div className={styles.card}>
           <h1 className={styles.title}>FIND OUR RESTAURANTS</h1>
           <p className={styles.text}>
-            1654 R. Don Road #304.
-            <br /> NewYork, 85022
+            Sector- 10, Road- 11
+            <br /> Uttara, 85022
             <br /> (602) 867-1010
           </p>
           <p className={styles.text}>
-            2356 K. Laquie Rd #235.
-            <br /> NewYork, 85022
+            Sector- 01, Road- 08
+            <br /> Mirpur, 85022
             <br /> (602) 867-1011
           </p>
           <p className={styles.text}>
-            1614 E. Erwin St #104.
-            <br /> NewYork, 85022
+            Sector- 05, Road- 03
+            <br /> Gulshan, 85022
             <br /> (602) 867-1012
           </p>
           <p className={styles.text}>
-            1614 W. Caroll St #125.
-            <br /> NewYork, 85022
+            Sector- 09, Road- 07
+            <br /> Motijheel, 85022
             <br /> (602) 867-1013
           </p>
         </div>
