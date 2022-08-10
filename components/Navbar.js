96602564aed70b282e9127a52c40a5fda0930ba1
unknown <rashadulislam4581@gmail.com>
2022-08-10 17:55:27 +0600

    today finished

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,8 +1,14 @@
 import Image from "next/image";
 import React from "react";
 import styles from "../styles/Navbar.module.css";
+import Link from "next/link";
+import { useSelector } from "react-redux";
+import { useRouter } from "next/router";
 
 const Navbar = () => {
+  const quantity = useSelector((state) => state.cart.quantity);
+  const { pathname } = useRouter();
+
   return (
     <div className={styles.container}>
       <div className={styles.item}>
@@ -16,20 +22,29 @@ const Navbar = () => {
       </div>
       <div className={styles.item}>
         <ul className={styles.list}>
-          <li className={styles.listItem}>Homepage</li>
-          <li className={styles.listItem}>Products</li>
+          <Link href="/" passHref>
+            <li className={styles.listItem}>Homepage</li>
+          </Link>
+          <Link
+            href={pathname === "/" ? "#products" : "/#products"}
+            passHref={false}
+          >
+            <li className={styles.listItem}>Products</li>
+          </Link>
           <li className={styles.listItem}>Menu</li>
           <Image src="/img/logo.png" alt="" width="160px" height="69px" />
           <li className={styles.listItem}>Blog</li>
           <li className={styles.listItem}>Contact</li>
         </ul>
       </div>
-      <div className={styles.item}>
-        <div className={styles.cart}>
-          <Image src="/img/cart.png" alt="" width="30px" height="30px" />
+      <Link href="/cart" passHref>
+        <div className={styles.item}>
+          <div className={styles.cart}>
+            <Image src="/img/cart.png" alt="" width="30px" height="30px" />
+          </div>
+          <div className={styles.counter}>{quantity}</div>
         </div>
-        <div className={styles.counter}>2</div>
-      </div>
+      </Link>
     </div>
   );
 };
--- components/PizzaList.js
+++ components/PizzaList.js
@@ -4,7 +4,7 @@ import PizzaCard from "./PizzaCard";
 
 const PizzaList = ({ pizzaList }) => {
   return (
-    <div className={styles.container}>
+    <div className={styles.container} id="products">
       <h1 className={styles.title}>THE BEST PIZZA IN TOWN</h1>
       <p className={styles.desc}>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut blandit arcu
