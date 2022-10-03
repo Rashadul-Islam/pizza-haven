0378d85a6a5a2c8470c3757700a222cfe1dcddcc
unknown <rashadulislam4581@gmail.com>
2022-10-03 14:53:54 +0600

    child error fixed

---
+++ pages/admin/addpizza.js
@@ -0,0 +1,163 @@
+import { useState } from "react";
+import styles from "../../styles/AddPizza.module.css";
+import axios from "axios";
+import Router from "next/router";
+
+const Addpizza = () => {
+  const [file, setFile] = useState(null);
+  const [title, setTitle] = useState(null);
+  const [desc, setDesc] = useState(null);
+  const [prices, setPrices] = useState([]);
+  const [extraOptions, setExtraOptions] = useState([]);
+  const [extra, setExtra] = useState(null);
+
+  const changePrice = (e, index) => {
+    const currentPrices = prices;
+    currentPrices[index] = e.target.value;
+    setPrices(currentPrices);
+  };
+
+  const handleExtraInput = (e) => {
+    setExtra({ ...extra, [e.target.name]: e.target.value });
+  };
+
+  const handleExtra = (e) => {
+    setExtraOptions((prev) => [...prev, extra]);
+  };
+
+  const handleCreate = async () => {
+    const data = new FormData();
+    data.append("file", file);
+    data.append("upload_preset", "uploads");
+    try {
+      const uploadRes = await axios.post(
+        "https://api.cloudinary.com/v1_1/pizza-haven/image/upload",
+        data
+      );
+
+      const { url } = uploadRes.data;
+      const newProduct = {
+        title,
+        desc,
+        prices,
+        extraOptions,
+        img: url,
+      };
+
+      await axios.post(
+        "https://pizza-haven.herokuapp.com/api/products",
+        newProduct
+      );
+
+      Router.push("/admin/managepizza");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <h1 className={styles.headerTitle}>Add a new Pizza</h1>
+        <div className={styles.item}>
+          <label className={styles.label}>Choose an image</label>
+          <input type="file" onChange={(e) => setFile(e.target.files[0])} />
+        </div>
+        <div className={styles.item}>
+          <label className={styles.label}>Title</label>
+          <input
+            className={styles.input}
+            type="text"
+            placeholder="Pizza name"
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+        <div className={styles.item}>
+          <label className={styles.label}>Desc</label>
+          <textarea
+            className={styles.textExtra}
+            placeholder="Pizza details...."
+            rows={8}
+            type="text"
+            onChange={(e) => setDesc(e.target.value)}
+          />
+        </div>
+        <div className={styles.item}>
+          <label className={styles.label}>Prices</label>
+          <div className={styles.priceContainer}>
+            <input
+              className={`${styles.input} ${styles.inputSm}`}
+              type="number"
+              placeholder="Small"
+              onChange={(e) => changePrice(e, 0)}
+            />
+            <input
+              className={`${styles.input} ${styles.inputSm}`}
+              type="number"
+              placeholder="Medium"
+              onChange={(e) => changePrice(e, 1)}
+            />
+            <input
+              className={`${styles.input} ${styles.inputSm}`}
+              type="number"
+              placeholder="Large"
+              onChange={(e) => changePrice(e, 2)}
+            />
+          </div>
+        </div>
+        <div className={styles.item}>
+          <label className={styles.label}>Extra</label>
+          <div className={styles.extra}>
+            <input
+              className={`${styles.input} ${styles.inputSm}`}
+              type="text"
+              placeholder="Item"
+              name="text"
+              onChange={handleExtraInput}
+            />
+            <input
+              className={`${styles.input} ${styles.inputSm}`}
+              type="number"
+              placeholder="Price"
+              name="price"
+              onChange={handleExtraInput}
+            />
+            <button className={styles.extraButton} onClick={handleExtra}>
+              Add
+            </button>
+          </div>
+          <div className={styles.extraItems}>
+            {extraOptions.map((option) => (
+              <span key={option.text} className={styles.extraItem}>
+                {option.text}
+              </span>
+            ))}
+          </div>
+        </div>
+        <button className={styles.addButton} onClick={handleCreate}>
+          Create
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export const getServerSideProps = async (ctx) => {
+  const myCookie = ctx.req?.cookies || "";
+  if (myCookie.token !== process.env.TOKEN) {
+    return {
+      redirect: {
+        destination: "/admin/login",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      show: true,
+    },
+  };
+};
+
+export default Addpizza;
